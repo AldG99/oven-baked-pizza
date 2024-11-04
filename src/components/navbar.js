73de3b00476367b1013b94050d6f5a38195e9e77
AldG99 <a.garciaglz17@gmail.com>
2024-11-04 11:49:06 -0600

    Agregar React Router para navegación entre páginas con el componente Layout

---
+++ src/components/navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from '../styles/navbar.module.css';
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}></div>
+      <div className={styles.item}></div>
+      <div className={styles.item}></div>
+    </div>
+  );
+};
+
+export default Navbar;
