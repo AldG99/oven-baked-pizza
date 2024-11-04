c9e7beb6cb5802a20d68bccd9600aebb4653fc23
AldG99 <a.garciaglz17@gmail.com>
2024-11-04 12:49:11 -0600

    Diseño de Navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,9 +4,48 @@ import styles from '../styles/navbar.module.css';
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.item}></div>
-      <div className={styles.item}></div>
-      <div className={styles.item}></div>
+      <div className={styles.item}>
+        <div className={styles.callButton}>
+          <img
+            src={require('../images/telephone.png')}
+            alt=""
+            width="32"
+            height="32"
+          />
+        </div>
+
+        <div className={styles.texts}>
+          <div className={styles.text}>ORDENAR AHORA!</div>
+          <div className={styles.text}>01 234 5678</div>
+        </div>
+      </div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Inicio</li>
+          <li className={styles.listItem}>Productos</li>
+          <li className={styles.listItem}>Menú</li>
+          <img
+            src={require('../images/logo.png')}
+            alt=""
+            width="160px"
+            height="69px"
+          />
+          <li className={styles.listItem}>Eventos</li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contacto</li>
+        </ul>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.cart}>
+          <img
+            src={require('../images/cart.png')}
+            alt=""
+            width="30px"
+            height="30px"
+          />
+          <div className={styles.counter}>2</div>
+        </div>
+      </div>
     </div>
   );
 };
