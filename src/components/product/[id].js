596f1cdd5a17b4d6f4f227a9817693f023dc8a2b
AldG99 <a.garciaglz17@gmail.com>
2024-11-18 14:05:49 -0600

    API Rest de Next.js con MongoDB

--- src/components/product/[id].js
+++ src/components/product/[id].js
@@ -3,7 +3,33 @@ import axios from 'axios';
 import styles from '../../styles/product.module.css';
 
 const Product = ({ pizza }) => {
+  const [price, setPrice] = useState(pizza.prices[0]);
   const [size, setSize] = useState(0);
+  const [quantity, setQuantity] = useState(1);
+
+  const [extras, setExtras] = useState([]);
+
+  const changePrice = number => {
+    setPrice(price + number);
+  };
+
+  const handleSize = sizeIndex => {
+    const difference = pizza.prices[sizeIndex] - pizza.prices[size];
+    setSize(sizeIndex);
+    changePrice(difference);
+  };
+
+  const handleChange = (e, option) => {
+    const checked = e.target.checked;
+
+    if (checked) {
+      changePrice(option.price);
+      setExtras(prev => [...prev, option]);
+    } else {
+      changePrice(-option.price);
+      setExtras(extras.filter(extra => extra._id !== option._id));
+    }
+  };
 
   return (
     <div className={styles.container}>
@@ -14,17 +40,17 @@ const Product = ({ pizza }) => {
       </div>
       <div className={styles.right}>
         <h1 className={styles.title}>{pizza.title}</h1>
-        <span className={styles.price}>${pizza.prices[size]}</span>
+        <span className={styles.price}>${price}</span>
         <p className={styles.desc}>{pizza.desc}</p>
         <h3 className={styles.choose}>Elige el tamaño</h3>
         <div className={styles.sizes}>
-          <button className={styles.sizeButton} onClick={() => setSize(0)}>
+          <button className={styles.sizeButton} onClick={() => handleSize(0)}>
             Pequeña
           </button>
-          <button className={styles.sizeButton} onClick={() => setSize(1)}>
+          <button className={styles.sizeButton} onClick={() => handleSize(1)}>
             Mediana
           </button>
-          <button className={styles.sizeButton} onClick={() => setSize(2)}>
+          <button className={styles.sizeButton} onClick={() => handleSize(2)}>
             Grande
           </button>
         </div>
@@ -34,16 +60,23 @@ const Product = ({ pizza }) => {
             <div className={styles.option} key={option._id}>
               <input
                 type="checkbox"
-                id="double"
-                name="double"
+                id={option.text}
+                name={option.text}
                 className={styles.checkbox}
+                onChange={e => handleChange(e, option)}
               />
               <label htmlFor="double">{option.text}</label>
             </div>
           ))}
         </div>
         <div className={styles.add}>
-          <input type="number" defaultValue={1} className={styles.quantity} />
+          <input
+            onChange={e => setQuantity(Number(e.target.value))}
+            type="number"
+            value={quantity}
+            min={1}
+            className={styles.quantity}
+          />
           <button className={styles.button}>Añadir al Carrito</button>
         </div>
       </div>
